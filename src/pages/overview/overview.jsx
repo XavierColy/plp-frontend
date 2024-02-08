da56a4be9d41c427c97d751b3b16df27093ade65
Xavier Coly <xavierfcoly@gmail.com>
2024-02-08 17:59:14 +0100

    feat: Ajout de la page d'accueil

--- src/pages/overview/overview.jsx
+++ src/pages/overview/overview.jsx
@@ -3,8 +3,8 @@ import './overview.css'
 import {FaCaretDown} from 'react-icons/fa';
 
 
-export default function Overview(){
-    return(
+export default function Overview() {
+    return (
         <div id={"overview"}>
             <SideBar></SideBar>
             <div id={"overview-main"}>
@@ -22,40 +22,51 @@ export default function Overview(){
                             <span className={"availability-footer"}>0</span>
                         </div>
                         <div id={"hosts"}>
-                            <div className={"host"}>
-                                <p>Nom</p>
-                                <p>Zabbix Server</p>
-                            </div>
-                            <div className={"host"}>
-                                <p>Nom</p>
-                                <p>Zabbix Server</p>
-                            </div>
-                            <div className={"host"}>
-                                <p>Nom</p>
-                                <p>Zabbix Server</p>
-                            </div>
-                            <div className={"host"}>
-                                <p>Nom</p>
-                                <p>Zabbix Server</p>
-                            </div>
-                            <div className={"host"}>
-                                <p>Nom</p>
-                                <p>Zabbix Server</p>
-                            </div>
-                            <div className={"host"}>
-                                <p>Nom</p>
-                                <p>Zabbix Server</p>
-                            </div>
-
+                            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => {
+                                return (
+                                    <div className={"host"}>
+                                        <p>Nom</p>
+                                        <p>Zabbix Server</p>
+                                    </div>);
+                            })}
                         </div>
                     </section>
                     <section>
                         <div id={"date-sort"} className={"card"}>
                             <span>31/01/2024</span>
-                            <FaCaretDown />
+                            <FaCaretDown/>
                         </div>
                         <div className={"card"} id={"alerts"}>
-                            <h3>Alertes</h3>
+                            <div>
+                                <h3>Alertes</h3>
+                                <span>Voir plus</span>
+                            </div>
+                            <table>
+                                <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Heure</th>
+                                    <th>Hôte</th>
+                                    <th>Problème</th>
+                                    <th>Durée</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map(() => {
+                                    return (
+                                        <tr>
+                                            <td className={"alert-level"}>
+                                                <div></div>
+                                            </td>
+                                            <td>10:51:52</td>
+                                            <td>Web Server</td>
+                                            <td>Linux: Number of installed packages has been changed</td>
+                                            <td>7h 4mn 12s</td>
+                                        </tr>
+                                    );
+                                })}
+                                </tbody>
+                            </table>
                         </div>
                     </section>
                 </main>
