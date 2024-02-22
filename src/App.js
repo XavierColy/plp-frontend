8e09c6837b966db1aef831acafcaec362d43323d
Xavier Coly <xavierfcoly@gmail.com>
2024-02-22 16:34:15 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import './App.css';
 import {Navigate, Route, Routes} from 'react-router-dom';
 import AuthProvider from './utils/authContext.context';
 import Overview from './pages/overview/overview';
-import HostPage from './pages/hostPage/nodePage';
 import Snmp from './pages/snmp/snmp';
 import Vm from './pages/vm/vm';
 import Windows from './pages/windows/windows';
 import LoginPage from './pages/login/loginPage';
 import AuthGuard from './utils/authGuard';
 import ChangePasswd from './pages/login/changePasswd';
+import NodePage from './pages/hostPage/nodePage';
 
 function App() {
     return (
         <AuthProvider>
             <div className="App">
                 <Routes>
-                    <Route path={"/"} element={<Navigate to={'/login'}/> }/>
+                    <Route path={"/"} element={<Navigate to={'/login'}/>}/>
                     <Route index path={'/login'} element={<LoginPage/>}/>
                     <Route element={<AuthGuard/>}>
                         <Route path={'/overview'} element={<Overview/>}/>
-                        <Route path={'/nodeExporter'} element={<HostPage/>}/>
+                        <Route path={'/nodeExporter'} element={<NodePage/>}/>
                         <Route path={'/snmpExporter'} element={<Snmp/>}/>
                         <Route path={'/vm'} element={<Vm/>}/>
                         <Route path={'/windows'} element={<Windows/>}/>
